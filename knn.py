7a2341ae1024aa421bb3c36398a2c4d1fd362c38
Matthew A Hernandez <mhernan0697@gmail.com>
2023-10-29 23:23:35 -0700

    detailed commit for knn pairs (knn and train)

--- knn.py
+++ knn.py
@@ -1,32 +1,37 @@
-
 import numpy as np
 from collections import Counter
 
-def euclidean_distance(x1,x2):
-	distance = np.sqrt(np.sum((x1-x2)**2))
-	return distance
+
+# def euclidean distance
+def euclidean_distance(x1, x2):
+	# distance between 2 feature vectors
+	return np.sqrt(np.sum((x1-x2)**2))
+
 
 class KNN:
-	def __init__(self,k=3):
+
+	def __init__(self, k):
 		self.k = k
 	
-	def fit(self, X, y):
-		self.X_train = X
-		self.y_train = y	
-
-	def predict(self, X):
-		predictions = [self._predict(x) for x in X]
-		return predictions	
+	# follow conventions from ML libraries
 	
-	def _predict(self, x):
-		# compute the distances
-		distances = [euclidean_distance(x, x_train) for x_train in self.X_train] 		
+	def fit(self, X, y):
+		# store training samples, X
+		self.X_train = X
+		self.y_train = y
+	
+	def predict(self, X):
+		predicted_labels = [self._predict(x) for x in X]
+		return np.array(predicted_labels)
 
-		# closest k
+	def _predict(self, x):
+		# helped method; only get 1 sample
+		# 1. Calculate Distance
+		distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+		# 2. Sort & count nearest samples, labels
 		k_indices = np.argsort(distances)[:self.k]
 		k_nearest_labels = [self.y_train[i] for i in k_indices]
-		
-		# majority vote
-		most_common = Counter(k_nearest_labels).most_common()
+		# 3. Do Majority vote
+		most_common = Counter(k_nearest_labels).most_common(1)
 		return most_common[0][0]
-
+		
--- train.py
+++ train.py
@@ -5,8 +5,6 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-from KNN import KNN
-
 cmap = ListedColormap(['#FF0000','#00FF00','#0000FF'])
 
 iris = datasets.load_iris()
@@ -14,15 +12,26 @@ X, y = iris.data, iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=1234)
 
-plt.figure()
-plt.scatter(X[:,2],X[:,3],c=y,cmap=cmap,edgecolor='k',s=20)
-plt.show()
+# 0. Look at your data
+# print (X_train.shape)
+# print(X_train[0])
+# # 120: # of samples 4: # of features
+# print(y_train.shape)
+# # 3 class problem; 0, 1, 2 red, green, blue
+# print(y_train) 
 
-clf = KNN(k=5)
+# 0.1 Plot your data
+# plt.figure()
+# # first two features X[:, 0] X[:, 1] 
+# plt.scatter(X[:,0],X[:,1],c=y,cmap=cmap,edgecolor='k',s=20)
+# plt.show()
+
+# KNN Implementation
+from knn import KNN
+clf = KNN(k=4)
 clf.fit(X_train,y_train)
 predictions = clf.predict(X_test)
 
-print(predictions)
 acc = np.sum(predictions == y_test) / len(y_test)
 print(acc)
 
