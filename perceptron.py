6942c8e4e82b6bd822c56c41113c763bf68cd13d
Matthew A Hernandez <mhernan0697@gmail.com>
2023-11-01 01:14:09 -0700

    added details about the perceptron algorithm underlining key features

--- perceptron.py
+++ perceptron.py
@@ -3,8 +3,13 @@
 import numpy as np
 
 
+
 def unit_step_func(x):
 	return np.where(x>0, 1, 0)
+
+# perceptron vs naive bayes
+# does not make strong independence assumptions involving features; must be linearly separable! (think xor)
+# discriminative linear classifier 
 
 class Perceptron:
 	
@@ -20,18 +25,20 @@ class Perceptron:
 		n_samples, n_features = X.shape
 
 		# init parameters
+		# online learning; weights change after examples; adding or subtracting
 		self.weights = np.zeros(n_features)
 		self.bias = 0
 		
 		y_ = np.where(x>0, 1, 0)
 	
-		# learn weights
+		# learn weights; look over epochs, or until convergence
 		for _ in range(self.n_iters):
 			for idx, x_i in enumerate(X):
+				# Make a prediction based on the current weights (+1, -1)
 				linear_output = np.dot(x_i, self.weights) + self.bias
 				y_predicted = self.activation_func(linear_output) 
 				
-				# perceptron update rule
+				# Update weights if an error was made 
 				update = self.lr * (y_[idx] - y_predicted)
 				self.weights +=  update * x_i
 				self.bias += update
